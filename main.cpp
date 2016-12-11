59b678e32ee32a35022d77cc3a578253b49d46f4
mameli <mameli93@gmail.com>
2016-12-11 16:50:49 +0100

    share

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ using namespace boost;
 
 int main ()
 {
-  unordered_map<string, int> hashMap;
-
+  unordered_map<string, int> hashMapWords;
+  unordered_map<string, int> hashMapLetters;
   string text = "blu rosso blu rosso verde blu verde rosso verde";
-  cout << text << '\n';
+  cout <<"Stringa di prova:\n"+text << '\n';
 
   vector<string> vTokens;
   vector<string>::iterator it;
@@ -26,27 +26,32 @@ int main ()
   boost::split(vTokens, text, boost::is_any_of(" "));
   for (it = vTokens.begin(); it != vTokens.end(); ++it) {
     itPeek = boost::next(it, 1);
-    if (*itPeek != "")
-      hashMap.emplace(*it+" "+*itPeek,0);
-    for (unsigned i = 0; i < it->length(); i += 2) {
+    if (*itPeek != "") hashMapWords.emplace(*it+" "+*itPeek,0);
+
+    for (unsigned i = 0; i < it->length(); ++i) {
       tokLetter = it->substr(i,2);
-      if (tokLetter.length()==2) {
-        std::cout << tokLetter << '\n';
-      }
+      if (tokLetter.length()==2) hashMapLetters.emplace(*it,0);
     }
 
   }
 
-  for ( auto itMap = hashMap.begin(); itMap != hashMap.end(); ++itMap ){
+  for ( auto itMap = hashMapLetters.begin(); itMap != hashMapLetters.end(); ++itMap ){
+    for (it = vTokens.begin(); it != vTokens.end(); ++it) {
+      itPeek = boost::next(it, 1);
+      if (itMap->first == *it+" "+*itPeek)
+          itMap->second++;
+  }
+
+  }  for ( auto itMap = hashMapWords.begin(); itMap != hashMapWords.end(); ++itMap ){
     for (it = vTokens.begin(); it != vTokens.end(); ++it) {
       itPeek = boost::next(it, 1);
       if (itMap->first == *it+" "+*itPeek)
           itMap->second++;
     }
   }
-
-  cout << "the hashmap contains:"<< std::endl;
-  for ( auto it = hashMap.begin(); it != hashMap.end(); ++it )
+  cout << "Bigrammi di parole" << '\n';
+  cout << "the hashMapWords contains:"<< std::endl;
+  for ( auto it = hashMapWords.begin(); it != hashMapWords.end(); ++it )
     cout << " " << it->first << ":" << it->second<< endl;
 
   return 0;
